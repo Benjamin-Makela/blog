75b09a36547fc0d21220d8e6bd487da528f731ea
Benjamin-Makela <benny.makela2008@gmail.com>
2025-03-01 18:19:05 -0800

    added cooldown to empty posts

--- src/thestuff.js
+++ src/thestuff.js
@@ -1,15 +1,24 @@
+const time = new Date();
+
 async function init() {
-    const postBtn = document.getElementById("post");
+    const postBtn = document.getElementById("postBtn");
     postBtn.addEventListener("click", post);
 }
 
+let emptyCldn = 0;
+let waitTime = 1000;
 function post() {
-    let field = document.getElementById("postField");
+    let field = document.getElementById("postBodyField");
     let text = field.value;
     if (text === "") {
-        field.placeholder = "YOU CANNOT POST NOTHING!";
-        setTimeout(function () {
-            field.placeholder = "type something";
-        }, 2000);
+        field.placeholder = "YOU CANNY POST NOTHING!";
+        emptyCldn++;
+        setTimeout(function() {
+            emptyCldn--;
+            if (emptyCldn == 0)
+                field.placeholder = "type something";
+        }, waitTime);
+    } else {
+        
     }
 }
