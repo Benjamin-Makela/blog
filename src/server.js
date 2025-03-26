507fb81148c35784b437c206787b090422d0c236
makelben-LAHS <100030354@mvla.net>
2025-03-26 09:45:55 -0700

    trying favicon working

--- src/server.js
+++ src/server.js
@@ -4,16 +4,29 @@ const path = require('path');
 const port = 80;
 
 const server = http.createServer(function (req, res) {
-    const filePath = path.join(__dirname, req.url === '/' ? 'benny.html' : req.url);
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end("<h1>404 Not Found</h1>");
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
-    });
+    if (req.url === "/") {
+        const filePath = path.join(__dirname, 'benny.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end("<h1>404 Not Found Because That Freaking File Does Not Exist Yo</h1>");
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === "/media/favicon.ico") {
+        const filePath = path.join(__dirname, "/media/favicon.ico");
+        fs.readFile(filePath, (err, data) => {
+            if (!err) {
+                res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("Not Found Because That Does Not Exist Yo");
+    }
 });
 
 server.listen(port, "0.0.0.0", function (error) {
