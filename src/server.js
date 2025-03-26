422d5f4bf63cf8e885316a2d77691169d28157d0
makelben-LAHS <100030354@mvla.net>
2025-03-26 08:57:16 -0700

    hope it works!

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const http = require("http");
 const port = 80;
 
-const server = http.createServer(function(req, res) {
-    res.write("hello node");
-    res.end();
+const server = http.createServer(function (req, res) {
+    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end("<h1>404 Not Found</h1>");
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
 });
 
-server.listen(port, "0.0.0.0", function(error) {
+server.listen(port, "0.0.0.0", function (error) {
     if (error) {
         console.log("somethin wenmt wrong", error);
     } else {
