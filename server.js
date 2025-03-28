6e1093df031cc13f4f0d11e82c3ffe91ba97c122
makelben-LAHS <100030354@mvla.net>
2025-03-28 11:33:29 -0700

    kdskas

--- server.js
+++ server.js
@@ -14,11 +14,10 @@ const server = http.createServer(function (req, res) {
             }
         });
     } else if (req.method === "POST") {
-        // console.log(req);
+        console.log(req);
         res.writeHead({ "Content-Type": "text/plain" });
         res.end();
     } else {
-        // console.log(req.method);
         const filePath = path.join(__dirname, req.url);
         fs.readFile(filePath, (err, data) => {
             // console.log(req.url);
